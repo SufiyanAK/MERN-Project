b613f126ce6ddcd80857a3f881efb24bdd029ee5
SufiyanAK <sufiyankhattak14@gmail.com>
2025-01-03 22:55:38 +0500

    feat: add product routes and update server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { ConnectDB } from '.src/config/db.js';
+import { ConnectDB } from './src/config/db.js';
+import productRoute from './src/routes/product.route.js';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
 })
+
+app.use('/api/products', productRoute)
 
 app.listen(port, () => {
     ConnectDB();
